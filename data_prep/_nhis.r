b95025b09661ffdb425df496fb05c896e2301a5e
dhdb19 <jordi.pagr@gmail.com>
2025-02-13 01:56:29 +0100

    .

--- data_prep/_nhis.r
+++ data_prep/_nhis.r
@@ -1,1 +1,10 @@
-read_sas
+library("SAScii")
+
+
+
+
+
+df1970 <- read.SAScii("data/1970_household/HOUSEHLD.DAT", "data/1970_household.sas", skip.decimal.division = TRUE)
+
+
+df1970 <- read_sas(data_file = "data/1970_household/HOUSEHLD.DAT", encoding = )
